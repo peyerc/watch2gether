39ea32afa1157531546556af3a4e74f1505a4604
Christian Bösch <christian.boesch@srf.ch>
2015-05-07 15:03:31 +0200

    First initial parsing of json

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
@@ -9,6 +12,41 @@ public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    /**
+     * Example Json:
+     *
+     * {
+     "channel": "SRF1",
+     "time": "123",
+     "show": "Fusball Bundesliga live"
+     }
+     * @return
+     */
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result saveEvent() {
+        JsonNode json = request().body().asJson();
+        JsonNode channelN = json.findValue("channel");
+        JsonNode timeN = json.findValue("time");
+
+        JsonNode showN = json.findValue("show");
+        if(channelN == null || timeN == null || showN == null) {
+            badRequest("A channel, time or show must be provided");
+        }
+
+        String channel = channelN.asText();
+        Long time = timeN.asLong();
+        String show = showN.asText();
+
+        //JsonNode numbers = json.findValue("numbers");
+
+
+
+        ObjectNode result = Json.newObject();
+        result.put("status", "ok");
+        result.put("show", show);
+        return ok(result);
     }
 
 }
